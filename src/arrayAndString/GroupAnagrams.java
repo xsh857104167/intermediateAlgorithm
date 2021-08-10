71ed5de9b1af5292d4362f5737e2d52cc6b5d20f
xsh857104167 <xsh857104167@163.com>
2021-08-10 15:33:40 +0800

    做完“字母异位词分组”的3种方法。

--- src/arrayAndString/GroupAnagrams.java
+++ src/arrayAndString/GroupAnagrams.java
@@ -2,9 +2,8 @@ package arrayAndString;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import javax.sql.rowset.spi.SyncResolver;
+import java.util.*;
 
 /**
  * @author Murphy Xu
@@ -51,9 +50,60 @@ public class GroupAnagrams {
         return res;
     }
 
+    /**
+     * 官方答案
+     * 排序 + map
+     * 6ms, 98.17%; 41.2MB, 71.42%
+     * @param strs
+     * @return
+     */
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str:strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    /**
+     * 官方答案
+     * 计数 + map
+     * 10ms, 33.30%; 41.8MB, 20.62%
+     * @param strs
+     * @return
+     */
+    public List<List<String>> groupAnagrams3(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str:strs) {
+            int[] counts = new int[26];
+            int length = str.length();
+            for (int i = 0; i < length; i++) {
+                counts[str.charAt(i) - 'a'] ++;
+            }
+            // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串作为哈希表的键
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < 26; i++) {
+                if (counts[i] != 0){
+                    sb.append((char)('a' + i));
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
     @Test
     public void test(){
         String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        System.out.println(groupAnagrams(strs));
+        System.out.println(groupAnagrams2(strs));
     }
 }
