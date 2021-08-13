02108febe675efa1c30486b092e5352968e8a7b7
xsh857104167 <xsh857104167@163.com>
2021-08-13 14:55:06 +0800

    做完“牛客合唱队”第二种方法，通过。

--- src/dp/Chorus.java
+++ src/dp/Chorus.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class Chorus {
     /**
      * 求最长合唱队
+     * 这种方式的思想好像有点瑕疵，先不用这个方式了
      * @param nums
      * @param len
      * @return
@@ -46,6 +47,43 @@ public class Chorus {
         return max;
     }
 
+    /**
+     * 求最长合唱队,
+     * 先正向求最大递增子序列，再逆向求最大上升子序列
+     * @param nums
+     * @param len
+     * @return
+     */
+    public int chorus2(int[] nums, int len){
+        int[] dp1 = new int[len];
+        int[] dp2 = new int[len];
+        dp1[0] = 1;
+        dp2[len -1] = 1;
+
+        for (int i = 1; i < len; i++) {
+            dp1[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]){
+                    dp1[i] = Math.max(dp1[i], dp1[j] + 1);
+                }
+            }
+        }
+
+        for (int i = len - 2; i >=0 ; i--) {
+            dp2[i] = 1;
+            for (int j = len - 1; j > i ; j--) {
+                if (nums[i] > nums[j]){
+                    dp2[i] = Math.max(dp2[i], dp2[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < len; i++) {
+            max = Math.max(max, dp1[i] + dp2[i]);
+        }
+        return max-1;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
@@ -56,7 +94,7 @@ public class Chorus {
                 nums[i] = in.nextInt();
             }
             Chorus test = new Chorus();
-            System.out.println(len - test.chorus(nums, len));
+            System.out.println(len - test.chorus2(nums, len));
         }
 
     }
