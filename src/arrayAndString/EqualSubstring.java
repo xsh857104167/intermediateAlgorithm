046295e81cb8b90d9831263c566f5c408a7b6230
xsh857104167 <xsh857104167@163.com>
2021-09-10 11:28:36 +0800

    做完补充题目1208尽可能使字符串相等。

---
+++ src/arrayAndString/EqualSubstring.java
@@ -0,0 +1,35 @@
+package arrayAndString;
+
+/**
+ * @author Murphy Xu
+ * @create 2021-09-10 11:17
+ */
+public class EqualSubstring {
+    /**
+     * 滑动窗口
+     * @param s
+     * @param t
+     * @param maxCost
+     * @return
+     */
+    public int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+        int[] diff = new int[n];
+        for (int i = 0; i < n; i++) {
+            diff[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        }
+        int maxLength = 0;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n){
+            sum += diff[end];
+            while (sum > maxCost){
+                sum -= diff[start];
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+            end++;
+        }
+        return maxLength;
+    }
+}
